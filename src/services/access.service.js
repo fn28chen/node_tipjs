c2c0c41eb40f5ebd1305e9ad250799a925a55d47
Ant4gon1st <PhongTN.B20VT288@stu.ptit.edu.vn>
2024-06-26 08:41:07 +0700

    Feat: Login

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -9,7 +9,11 @@ const { getInfoData } = require("../utils");
 const {
   BadRequestError,
   ConflictRquestError,
+  AuthFailureError,
 } = require("../core/error.response");
+
+
+const { findByEmail } = require("./shop.service");
 
 const RoleShop = {
   SHOP: "SHOP",
@@ -41,17 +45,19 @@ class AccessService {
     // step 3: Create privateKey, publicKey and save collection keyStore
     if (newShop) {
       // created privateKey, publicKey
-      const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 4096,
-        publicKeyEncoding: {
-          type: "pkcs1",
-          format: "pem",
-        },
-        privateKeyEncoding: {
-          type: "pkcs1",
-          format: "pem",
-        },
-      });
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      // });
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
       console.log({ privateKey, publicKey }); // save collection keyStore
       const publicKeyString = await KeyTokenService.createKeyToken({
         userId: newShop._id,
@@ -99,6 +105,56 @@ class AccessService {
     //   return { code: "xxx", message: error.message, status: error };
     // }
   };
+
+  
+  static login = async ({ email, password, refreshToken = null}) => {
+    // step 1: Check email exist?
+    const foundShop = await findByEmail({ email });
+    if(!foundShop) throw new BadRequestError("Error: Shop not found!");
+
+    // step 2: Match password
+    const match = bcrypt.compare(password, foundShop.password);
+    if(!match) throw new AuthFailureError("Error: Auth error!");
+    
+    // step 3: Create token pair and save
+    const privateKey = crypto.randomBytes(64).toString("hex");
+    const publicKey = crypto.randomBytes(64).toString("hex");
+    const publicKeyString = await KeyTokenService.createKeyToken({
+      userId: foundShop._id,
+      publicKey,
+    });
+    if (!publicKeyString) {
+      return {
+        code: "xxxx",
+        message: "Create publicKey fail",
+      };
+    }
+    // step 4: Generate token pair
+    const { _id: userId } = foundShop;
+    const tokens = await createTokenPair(
+      {
+        userId,
+        email,
+      },
+      publicKeyString,
+      privateKey
+    );
+
+    await KeyTokenService.createKeyToken({
+      refreshToken: tokens.refreshToken,
+      publicKey,
+      privateKey,
+      userId
+    })
+
+    return {
+        shop: getInfoData({
+          fields: ["_id", "name", "email"],
+          object: foundShop,
+        }),
+        tokens,
+    };
+  }
 }
 
 module.exports = AccessService;
